bd27fc588da6f92159cda5149c6af91048fb3341
Phu Nguyen <phunguyen@b13technology.com>
2021-07-18 19:03:48 +0700

    init nabbar

---
+++ src/components/ui/Header.tsx
@@ -0,0 +1,12 @@
+import { AppBar, Toolbar } from '@material-ui/core'
+import React from 'react'
+
+export default function Header() {
+  return (
+    <AppBar>
+      <Toolbar>
+        Arc development
+      </Toolbar>
+    </AppBar>
+  )
+}
